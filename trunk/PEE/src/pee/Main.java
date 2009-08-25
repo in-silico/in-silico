133225df0b582918b13103dab3da1447a9ebbfb6
sebasutp <sebasutp@13b141fe-a120-11dd-a9c7-49dec45ca499>
2009-08-25 18:33:51 +0000

    Archivos de JFlex y Cup(Yacc)

--- trunk/PEE/src/pee/Main.java
+++ trunk/PEE/src/pee/Main.java
@@ -5,6 +5,13 @@
 
 package pee;
 
+import java.io.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java_cup.runtime.Symbol;
+import pee.compilador.*;
+
+
 /**
  *
  * @author sebas
@@ -15,7 +22,14 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        try {
+            // TODO code application logic here
+            scanner s = new scanner(new FileInputStream("test.pee"));
+            parser p = new parser(s);
+            p.parse();
+        } catch (Exception ex) {
+            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 
 }
