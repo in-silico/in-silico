9bf8a4610158907b9588782b91d4fe8d447636a1
santigutierrez1 <santigutierrez1@13b141fe-a120-11dd-a9c7-49dec45ca499>
2010-01-07 07:14:41 +0000

    Se agrega el modo edición, para lo cual se tuvo que utilizar multithreading. Ya solo falta la marca de agua para el fondo, que tenga que ver con hormigas.

--- trunk/Proyectos/src/implementacion/hormigas/ModeloHormigasTSP.java
+++ trunk/Proyectos/src/implementacion/hormigas/ModeloHormigasTSP.java
@@ -3,25 +3,31 @@ package implementacion.hormigas;
 import genericos.hormigas.Hormigas;
 import genericos.hormigas.ModeloHormigas;
 
+import java.awt.BorderLayout;
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Random;
+import java.util.Scanner;
+
+import javax.swing.ButtonGroup;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JRadioButton;
 
 public class ModeloHormigasTSP implements ModeloHormigas
 {
     static int M = 100; 
     static int Q = 1;   
-    static int inicial = 3000;
+    static int inicial = 4000;
     static int numIteraciones = 300000;
     static double a = 0.1;    
     static double b = 5;
     static double p = 0.001;
     static double e = 0.1;
     static Random azar = new Random();
-    
-    Grafico grafico;
     
 	public int escoger(double [] m)
 	{
@@ -206,6 +212,10 @@ public class ModeloHormigasTSP implements ModeloHormigas
 
 	public int darNumeroIteraciones()
 	{
+		if(Grafico.este.termino())
+		{
+			return 0;
+		}
 		return numIteraciones;
 	}
 
@@ -222,22 +232,168 @@ public class ModeloHormigasTSP implements ModeloHormigas
 	@SuppressWarnings("unchecked")
 	public static void main(String[] args) throws FileNotFoundException, IOException, ClassNotFoundException
 	{
-		Hormigas h = new Hormigas();
-	    Grafico g = new Grafico();
-		ModeloHormigasTSP modelo = new ModeloHormigasTSP();
+		final Grafico g = new Grafico();
+		Grafico.este = g;
 		Object[] entrada = Lectura.leer();
-		modelo.grafico = g;
-		g.ciudades = (ArrayList <Ciudad>) entrada[0];
-		g.setVisible(true);
-		h.solucionar((double[][]) entrada[1], modelo);
+		HiloGrafico hg = new HiloGrafico((ArrayList <Ciudad>) entrada[0]);
+		new Thread(hg).start();
 	}
 
 	public void establecerMejorActual(ArrayList <Integer> mejorSolucion, double pesoMejor) 
 	{
-		grafico.mejoresActual = mejorSolucion;
-		grafico.solucionActual = pesoMejor;
-		System.out.println(pesoMejor);
-		grafico.repaint();
+		Grafico.este.establecerMejorActual(mejorSolucion, pesoMejor);
 	}
+	
+	public static class HiloHormiga implements Runnable 
+	{
+		ModeloHormigas modelo;
+		double[][] matrizDistancias;
+		
+		@Override
+		public void run() 
+		{
+			Hormigas h = new Hormigas();
+			h.solucionar(matrizDistancias, modelo);
+		}
+	}
+	
+	public static class HiloGrafico implements Runnable
+	{
+		ArrayList<Ciudad> ciudades;
+		public HiloGrafico(ArrayList <Ciudad> ciudades) 
+		{
+			this.ciudades = ciudades;
+		}
 
+		@Override
+		public void run() 
+		{
+			Grafico.este.ciudades = ciudades;
+			FramePrincipal principal = new FramePrincipal();
+			principal.setVisible(true);
+		}
+	}
+	
+	static class FramePrincipal extends JFrame
+	{
+		private static final long serialVersionUID = -4984727444905699787L;
+
+		public FramePrincipal()
+		{
+			super("Colonia de hormigas");
+			setBounds(50, 50, 500, 500);
+			ButtonGroup bg = new ButtonGroup();
+		    JRadioButton modoEdicion = new JRadioButton("Modo edicion");
+		    JRadioButton modoSolucion = new JRadioButton("Modo solucion");
+		    bg.add(modoEdicion);
+		    bg.add(modoSolucion);
+		    bg.setSelected(modoEdicion.getModel(), true);
+		    modoEdicion.addActionListener(Grafico.este);
+		    modoSolucion.addActionListener(Grafico.este);
+		    JPanel panel = new JPanel();
+		    panel.add(modoEdicion);
+		    panel.add(modoSolucion);
+		    setLayout(new BorderLayout());
+		    add(panel, BorderLayout.SOUTH);
+		    add(Grafico.este, BorderLayout.CENTER);
+		}
+	}
 }
+
+class Ciudad
+{
+	int x;
+	int y;
+}
+
+class Lectura 
+{
+	public static Object[] leer()
+	{
+		Scanner sc;
+		try {
+			sc = new Scanner(new File("src/implementacion/hormigas/Cities.xml"));
+		} catch (FileNotFoundException e) {
+			sc = null;
+		}
+		sc.nextLine();
+		sc.nextLine();
+		ArrayList <Ciudad> ciudades = new ArrayList <Ciudad> ();
+		while(sc.hasNextLine())
+		{
+			boolean primero = false;
+			String primeroS = "";
+			String segundoS = "";
+			boolean segundoInicial = false;
+			boolean segundo = false;
+			String a = sc.nextLine();
+			if(a.equals("</CityList>"))
+				break;
+			for(char c : a.toCharArray())
+			{
+				if(!primero)
+				{
+					if(c == '"')
+					{
+						primero = true;
+						continue;
+					}
+				}
+				if(primero && !segundoInicial && !segundo)
+				{
+					if(c == '"')
+					{
+						segundoInicial = true;
+						primero = true;
+						continue;
+					}
+					primeroS += c;
+				}
+				if(segundoInicial && !segundo)
+				{
+					if(c == '"')
+					{
+						segundoInicial = false;
+						segundo = true;
+						continue;
+					}
+				}
+				if(segundo)
+				{
+					if(c == '"')
+					{
+						break;
+					}
+					segundoS += c;
+				}
+			}
+			Ciudad ci = new Ciudad();
+			ci.x = Integer.parseInt(primeroS);
+			ci.y = Integer.parseInt(segundoS);
+			ciudades.add(ci);
+		}
+		double [][] datosPrueba = generarDistancias(ciudades);
+		Object[] retorno = {ciudades, datosPrueba};
+		return retorno;
+	}
+	
+	public static double[][] generarDistancias(ArrayList <Ciudad> ciudades)
+	{
+		double [][] datosPrueba = new double[ciudades.size()][ciudades.size()];
+		for(int i = 0; i < ciudades.size(); i++)
+			for(int j = 0; j < ciudades.size(); j++)
+			{
+				if(i == j)
+				{
+					datosPrueba[i][j] = Double.POSITIVE_INFINITY;
+				}
+				else
+				{
+					datosPrueba[i][j] = Math.sqrt((ciudades.get(i).x - ciudades.get(j).x) * (ciudades.get(i).x - ciudades.get(j).x) + (ciudades.get(i).y - ciudades.get(j).y) * (ciudades.get(i).y - ciudades.get(j).y));
+				}
+			}
+		return datosPrueba;
+	}
+}
+
+
