0ac4448207de70e970290f5b948a2d33f785ca92
sebasutp <sebasutp@13b141fe-a120-11dd-a9c7-49dec45ca499>
2010-01-14 03:36:42 +0000

    Terminada la página de noticias sin el diseño y corrección del logueo en index.php

--- trunk/PaginaWeb/src/privado/noticias.php
+++ trunk/PaginaWeb/src/privado/noticias.php
@@ -1,25 +1,55 @@
 
 <?php
+	include("../conn.php");	
 	if ($_POST['submit'] ) {
 		$titulo = $_POST['titulo'];
 		$link = $_POST['link'];
 		$imagen = "imagenes/".$_FILES["file"]["name"];
 		$resumen = $_POST['resumen'];
+		$mostrar = ($_POST['mostrar']) ? 1 : 0;
 		
-		include("../conn.php");		
 		if ($_GET['idNoticia']) {
 			$idNot=$_GET['idNoticia'];
 			if ($_FILES["file"]["name"]) {
 				mysql_query("Update Noticias Set Titulo='$titulo', Link='$link', Imagen='$imagen', Resumen='$resumen' where idNoticia=$idNot");
 				move_uploaded_file($_FILES["file"]["tmp_name"], "../imagenes/".$_FILES["file"]["name"]);
 			} else {
-				mysql_query("Update Noticias Set Titulo='$titulo', Link='$link', Resumen='$resumen' where idNoticia=$idNot");
+				mysql_query("Update Noticias Set Titulo='$titulo', Link='$link', Resumen='$resumen', Mostrar=$mostrar where idNoticia=$idNot");
 			}
 		} else {
-			mysql_query("Insert into Noticias (Resumen, Titulo, Imagen, Link, Mostrar) values ('$resumen', '$titulo', '$imagen', '$link', 1)");
+			mysql_query("Insert into Noticias (Resumen, Titulo, Imagen, Link, Mostrar) values ('$resumen', '$titulo', '$imagen', '$link', $mostrar)");
 			move_uploaded_file($_FILES["file"]["tmp_name"], "../imagenes/".$_FILES["file"]["name"]);
 		}
 	}
+	if ($_GET['idBorrar']) {
+		$idNot=$_GET['idBorrar'];
+		$result = mysql_query("Select Imagen from Noticias where idNoticia=$idNot");
+		$row = mysql_fetch_array($result);
+		$imgFile = $row["Imagen"];
+		unlink("../".$imgFile);
+		mysql_query("Delete from Noticias where IdNoticia=$idNot");		
+	}
+?>
+
+<table>
+<?php
+	include("../conn.php");	
+	$result = mysql_query("Select idNoticia, Titulo, Mostrar from Noticias");
+	echo "<tr> <td>Titulo</td> <td>Mostrar</td> <td>Comandos</td> </tr>\n";
+	while ($row = mysql_fetch_array($result)) {
+		$idNot=$row["idNoticia"];
+		$titulo=$row["Titulo"];
+		$mostrar=$row["Mostrar"];
+		$editar="<a href=\"noticias.php?idNoticia=$idNot\">editar</a>";
+		$borrar="<a href=\"noticias.php?idBorrar=$idNot\">borrar</a>";
+		echo "<tr> <td>$titulo</td> <td>$mostrar</td> <td>$editar&nbsp$borrar</td> </tr>\n";
+	}
+?>
+</table>
+<br />
+
+
+<?php
 	if ($_GET['idNoticia']) {
 		include("../conn.php");
 		$idNot=$_GET['idNoticia'];
@@ -45,7 +75,9 @@
 	Link: <input type="text" name="link" value=<?php echo "'".$row["Link"]."'" ?> /> <br />
 	Imagen: <input type="file" name="file" id="file" file=<?php echo "'".$row["Imagen"]."'" ?> /> <br />
 	Resumen: <textarea name="resumen"><?php echo $row["Resumen"] ?></textarea> <br />
-	<input type="submit" value="Enviar" name="submit" />
+	Mostrar: <input type="checkbox" name="mostrar" <?php if($row["Mostrar"]==1) echo "checked"; ?> />
+	<br /><input type="submit" value="Enviar" name="submit" />
+	<input type="button" value="Nuevo" name="nuevo" onclick='window.location="./noticias.php"' />
 </form>
 
 
