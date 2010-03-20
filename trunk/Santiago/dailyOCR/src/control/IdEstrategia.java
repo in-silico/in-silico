64731a4cefeeab9919b62a7d195b4149594dcbc0
santigutierrez1 <santigutierrez1@13b141fe-a120-11dd-a9c7-49dec45ca499>
2010-03-20 19:07:50 +0000

    Reestructuracion del codigo dailyOCR/FX

--- trunk/Santiago/dailyOCR/src/control/IdEstrategia.java
+++ trunk/Santiago/dailyOCR/src/control/IdEstrategia.java
@@ -2,7 +2,7 @@ package control;
 
 public enum IdEstrategia
 {
-	BREAKOUT1, BREAKOUT2, RANGE1, RANGE2, MOMENTUM1, MOMENTUM2, TECHNICAL, JOEL, JOELRECOMENDACIONES;
+	BREAKOUT1, BREAKOUT2, RANGE1, RANGE2, MOMENTUM1, MOMENTUM2, TECHNICAL, JOEL;
 	
 	public static IdEstrategia darEstrategia(int id)
 	{
