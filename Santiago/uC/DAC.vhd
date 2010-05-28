b0757e6b87e69427696f2cf1e976b1028b4938ba
sebasutp <sebasutp@users.noreply.github.com>
2010-05-28 00:00:34 +0000

    SPI controller

--- Santiago/uC/DAC.vhd
+++ Santiago/uC/DAC.vhd
@@ -1,22 +1,4 @@
-----------------------------------------------------------------------------------
--- Company: 
--- Engineer: Sebastián Gómez
--- 
--- Create Date:    14:15:10 04/26/2010 
--- Design Name: 
--- Module Name:    DAC - DAC_Arq 
--- Project Name: 
--- Target Devices: 
--- Tool versions: 
--- Description: 
---
--- Dependencies: 
---
--- Revision: 
--- Revision 0.01 - File Created
--- Additional Comments: 
---
-----------------------------------------------------------------------------------
+
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.STD_LOGIC_ARITH.ALL;
@@ -28,14 +10,16 @@ use IEEE.STD_LOGIC_UNSIGNED.ALL;
 --use UNISIM.VComponents.all;
 
 entity DAC is
-    Port ( data : in  STD_LOGIC_VECTOR (11 downto 0);
-           spi_sck : in  STD_LOGIC;
-           da_conv : in  STD_LOGIC;
-           addr : in  STD_LOGIC_VECTOR (3 downto 0);
+    Port ( dataIn : in  STD_LOGIC_VECTOR (31 downto 0);
+           dataOut : out  STD_LOGIC_VECTOR (31 downto 0);
+           configIn : in  STD_LOGIC_VECTOR(7 downto 0);
            spi_miso : in  STD_LOGIC;
-           ready : out  STD_LOGIC;
            spi_mosi : out  STD_LOGIC;
-           dac_cs : out  STD_LOGIC);
+			  spi_clk : out STD_LOGIC;
+			  spi_cs : out STD_LOGIC;
+			  ready : out  STD_LOGIC;
+			  clk : in STD_LOGIC
+           );
 end DAC;
 
 architecture DAC_Arq of DAC is
@@ -43,25 +27,47 @@ architecture DAC_Arq of DAC is
 	signal sdata : std_logic_vector(31 downto 0) := x"00000000";
 	signal estado : std_logic_vector(5 downto 0) := "000000";
 	
+	constant frec_div : std_logic_vector(3 downto 0) := "0101"; --const 10MHz
+	signal frec_cnt : std_logic_vector(3 downto 0) :="0000";
+	signal spi_sck : std_logic:='0';
+	signal currBit : std_logic_vector(4 downto 0):="00000";
+	
 	signal command : std_logic_vector(3 downto 0);
-	signal currBit : std_logic_vector(4 downto 0);
+	signal da_conv : std_logic;
 begin
-	-- constantes
-	command <= "0011";
 	
 	--señales
-	dac_cs <= sready;
+	spi_cs <= sready;
 	ready <= sready;
 	currBit <= estado(4 downto 0); -- byte actual: 4 bits menos sig de estado
+	spi_clk <= spi_sck;
+	sdata <= dataIn;
+	spi_clk <= spi_sck;
+	
+	command <= configIn(3 downto 0);
+	da_conv <= configIn(4);
+	
+	process (clk)
+	begin
+		if rising_edge(clk) then
+			if frec_cnt=frec_div then
+				frec_cnt<="0001";
+				spi_sck <= not spi_sck;
+			else
+				frec_cnt <= frec_cnt+1;
+			end if;
+		end if;
+	end process;
+	
 	
 	process (spi_sck)
 	begin
 		
 		if (falling_edge(spi_sck)) then
 			--sready: indica si el conversor está listo para recibir un dato nuevo
-			if (sready='1' and da_conv='1') then
+			if (sready='1' and da_conv='1' and command="0011") then
 				sready <= '0';
-				sdata <= "00000000" & command & addr & data & "0000";
+				sdata <= dataIn;
 				estado <= "111110";
 				spi_mosi <= '0';
 			end if;
