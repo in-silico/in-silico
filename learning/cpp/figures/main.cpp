0e3c392332f9cf9b8ffffd147c8819e98d26253e
sebasutp <sebasutp@users.noreply.github.com>
2010-07-16 02:20:25 +0000

    Lectura de entrenamiento de imágenes de carpeta training

--- learning/cpp/figures/main.cpp
+++ learning/cpp/figures/main.cpp
@@ -5,150 +5,44 @@
  * Created on 25 de junio de 2010, 02:59 PM
  */
 
-#include <cstdlib>
-#include <cstdio>
-#include <cxtypes.h>
-#include <highgui.h>
-#include <cv.h>
-#include "lineal.h"
+
+#include "io_utils.h"
+#include "features.h"
 #include <ml.h>
 
-#define CAM 1
-#define WIN "Ventana1"
-#define MAXL 10
-#define MINL 2
-#define COLS 4
+#define DIR_TR "./training/"
 
-
-CvCapture* cap;
-CvMemStorage *seq, *poly;
-char fig; //figura del test case
-char *fname;
-bool mostrar=false;
 
 CvDTree* ptree=0;
 
-void init_params() {
-    if (mostrar) cvNamedWindow(WIN, CV_WINDOW_AUTOSIZE);
-    cap = cvCreateCameraCapture(CAM);
-    seq = cvCreateMemStorage();
-    poly = cvCreateMemStorage();
-}
-
-void destroy_params() {
-    cvReleaseCapture(&cap);
-    //cvDestroyWindow(WIN);
-    cvReleaseMemStorage(&seq);
-    cvReleaseMemStorage(&poly);
-}
-
-IplImage* captureAndFilter(IplImage *predict=0) {
-    IplImage* frame = 0;
-    if (predict != 0)
-        frame = predict;
-    else if (fig == '\0')
-        frame = cvQueryFrame(cap);
-    else
-        frame = cvLoadImage( fname );
-    if (mostrar) {
-        cvShowImage(WIN, frame);
-        cvWaitKey();
-    }
-    IplImage *img = cvCreateImage(cvSize(frame->width,frame->height),IPL_DEPTH_8U,1);
-    cvCvtColor(frame,img,CV_RGB2GRAY);
-    cvSmooth(img,img);
-    cvAdaptiveThreshold(img, img, 255, CV_ADAPTIVE_THRESH_MEAN_C, CV_THRESH_BINARY,15);
-    cvDilate(img,img);
-    cvErode(img,img);
-    if (mostrar) {
-        cvShowImage(WIN, img);
-        cvWaitKey();
-    }
-    return img;
-}
-
-double mean(double *data, int n) {
-    double u=0;
-    for (int i=0; i<n; i++) {
-        u += data[i];
-    }
-    return u/n;
-}
-
-double desv_est(double * data, double media, int n) {
-    double u=0, tmp;
-    for (int i=0; i<n; i++) {
-        tmp = (data[i]-media);
-        u += tmp*tmp;
-    }
-    return sqrt(u/n);
-}
-
-void printFeatures(CvSeq* poly, float *ans=0) {
-    int nlados = poly->total;
-        
-    double lados[MAXL];
-    double ang[MAXL];
-    double points[MAXL][DIMS];
-    double vects[MAXL][DIMS];
-    double maxang=0;
-    for (int j=0; j<nlados; j++) {
-        CvPoint* p = CV_GET_SEQ_ELEM( CvPoint, poly, j );
-        points[j][0] = p->x;
-        points[j][1] = p->y;
-    }
-    for (int j=0; j<nlados; j++) {
-        diff(vects[j], points[j], points[(j+1) % nlados]);
-        lados[j] = norm(vects[j]);
-    }
-    for (int j=0; j<nlados; j++) {
-        ang[j] = 180-vect_ang(vects[j],vects[(j+1)%nlados]);
-        maxang = max(maxang,ang[j]);
-    }
-    double m_lados,m_ang,d_lados,d_ang;
-    m_lados=mean(lados,nlados); m_ang=mean(ang,nlados);
-    d_lados=desv_est(lados,m_lados,nlados); d_ang=desv_est(ang,m_ang,nlados);
-    
-    if ( ans!=0 ) {
-        ans[0]=nlados;
-        ans[1]=d_lados/m_lados;
-        ans[2]=d_ang/m_ang;
-        ans[3]=maxang;
-    } else {
-        printf("%c %i %lf %lf %lf\n",fig,nlados,d_lados/m_lados,d_ang/m_ang,maxang);
-    }
-}
-
-void getFeatures(IplImage *predict=0, float *features=0) {
-    IplImage *img = captureAndFilter(predict);
-    cvClearMemStorage(seq);
-    CvSeq* cont = 0, *p2=0;
-    int Nc = cvFindContours(img,seq,&cont,sizeof(CvContour),CV_RETR_LIST);
-    //p2 = cvApproxPoly(cont,sizeof(CvContour),poly,CV_POLY_APPROX_DP,20);
-    if ( cont ) {
-        for (CvSeq *i=cont; i!=NULL; i=i->h_next) {
-            cvZero(img);
-            p2 = cvApproxPoly(i,sizeof(CvContour),poly,CV_POLY_APPROX_DP,10);
-
-            int nlados = p2->total;
-            if (nlados >= MAXL || nlados <= MINL) continue;
-
-            if (mostrar) {
-                cvDrawContours(img,p2,cvScalarAll(255),cvScalarAll(255),0);
-                cvShowImage(WIN,img);
-                cvWaitKey();
+void printMatrix(CvMat * mat, int type=CV_32F) {
+    int N = mat->rows;
+    int M = mat->cols;
+    if (type == CV_32F) {
+        float *fdata;
+        for (int i=0; i<N; i++) {
+            fdata = mat->data.fl + i*M;
+            for (int j=0; j<M; j++) {
+                printf("%f\t", fdata[j]);
             }
-
-            printFeatures(p2, features);
-            if (features != 0) break;
+            printf("\n");
+        }
+    } else if (type == CV_32S) {
+        int *idata;
+        for (int i=0; i<N; i++) {
+            idata = mat->data.i + i*M;
+            for (int j=0; j<M; j++) {
+                printf("%i\t", idata[j]);
+            }
+            printf("\n");
         }
     }
-    //printf("El número de contornos es: %i\n",Nc);
 }
 
-void train() {
+void trainFromTxt() {
     FILE* fin = fopen("train.txt","r");
     int N,i;
+    training = true;
     fscanf(fin,"%i",&N);
     CvMat *data = cvCreateMat(N,COLS,CV_32F);
     CvMat *resp = cvCreateMat(N,1,CV_32F);
@@ -164,14 +58,73 @@ void train() {
         *fresp = (float)c[0];
     }
 
+    printMatrix(data);
+    printMatrix(resp);
+
     CvMat *vartype = cvCreateMat( data->cols + 1, 1, CV_8U );
+    unsigned char *vtype = vartype->data.ptr;
+
+    //Tipos de variables de entrada al árbol
+    vtype[0]=CV_VAR_NUMERICAL;
+    vtype[1]=CV_VAR_NUMERICAL;
+    vtype[2]=CV_VAR_NUMERICAL;
+    vtype[3]=CV_VAR_NUMERICAL;
+
+    vtype[4]=CV_VAR_CATEGORICAL; //Tipo de la salida del árbol
+
     ptree = new CvDTree;
     ptree->train(data,CV_ROW_SAMPLE,resp,0,0,vartype,0,CvDTreeParams());
     
 }
 
+bool str_ends_with(const char *cad, const char *end) {
+    int n = strlen(cad), m = strlen(end);
+    if (n<m) return false;
+    const char *end2 = cad+(n-m);
+    return ( strcmp(end,end2)==0 );
+}
+
+void training_image(const char *filename) {
+    char fname[256];
+    if (str_ends_with(filename,".jpg")) {
+        fig = filename[0];
+        strcpy(fname,DIR_TR);
+        strcat(fname, filename);
+        getFeaturesFN( fname );
+    }
+}
+
+
+/**
+ * Creates a matrix for training but feeds it from images taken of folder
+ * ./training; Only JPG images are taken into account and all shapes in those
+ * images are clasified according to the first letter of the picture.
+ */
+void train() {
+    training = true;
+    listFiles(DIR_TR,training_image);
+    fillMatrix();
+
+    //printMatrix(t_data);
+    //printMatrix(t_resp);
+
+    CvMat *vartype = cvCreateMat( t_data->cols + 1, 1, CV_8U );
+    unsigned char *vtype = vartype->data.ptr;
+
+    //Tipos de variables de entrada al árbol
+    vtype[0]=CV_VAR_NUMERICAL;
+    vtype[1]=CV_VAR_NUMERICAL;
+    vtype[2]=CV_VAR_NUMERICAL;
+    vtype[3]=CV_VAR_NUMERICAL;
+
+    vtype[4]=CV_VAR_CATEGORICAL; //Tipo de la salida del árbol
+
+    ptree = new CvDTree;
+    ptree->train(t_data,CV_ROW_SAMPLE,t_resp,0,0,vartype,0,CvDTreeParams());
+}
+
 void predict() {
-    
+    training=false;
     IplImage* frame;
     while (1) {
         frame = cvQueryFrame(cap);
@@ -200,15 +153,9 @@ int main(int argc, char** argv) {
     //destroy_params();
     init_params();
     argc--; argv++;
-    if (argc > 0) {
-        fig = argv[0][0];
-        fname = argv[1];
-        getFeatures();
-    } else {
-        train();
-        predict();
-    }
-
+    train();
+    //trainFromTxt();
+    predict();
     destroy_params();
     return (EXIT_SUCCESS);
 }
