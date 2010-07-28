4d9d620f7efcc241ec17c4976635c3141a677c1a
sebasutp <sebasutp@13b141fe-a120-11dd-a9c7-49dec45ca499>
2010-07-28 16:00:27 +0000

    Terminada y probada la multiplicación, la división por un único digito se terminó pero no se ha probado bien.

--- trunk/no-java/c/BigNum/BigInt.h
+++ trunk/no-java/c/BigNum/BigInt.h
@@ -5,16 +5,14 @@
  * Created on 25 de mayo de 2010, 03:40 PM
  */
 
-#include "Macros.h"
 #include <stdlib.h>
+#include <stdio.h>
+#include "bnUtils.h"
 
 #define WBITS 32
 #define WMASK ( ((dword)1 << WBITS) - 1
 #define BN_NEG 0
 #define BN_POS 1
-
-typedef unsigned int word;
-typedef unsigned long dword;
 
 /**
  * Structure that represents big integers, d[0] is the least significant
@@ -27,11 +25,15 @@ typedef struct {
     word *d; //value of each digits
 } BigInt;
 
+void printHex(BigInt *a);
 BigInt* bnNewBigInt(word size, word initVal);
 void bnDelBigInt(BigInt *a);
 int bnCompareInt(BigInt *a, BigInt *b);
 void bnNegInt(BigInt *a);
+void bnShiftLBits(BigInt *res, BigInt *a, word bits);
 void bnAddInt(BigInt *res, BigInt *a, BigInt *b);
 void bnSubInt(BigInt *res, BigInt *a, BigInt *b);
-//void bnMulInt(BigInt *res, BigInt *a, BigInt *b);
+void bnMulInt(BigInt *res, BigInt *a, BigInt *b);
+void bnDivIntWord(BigInt *ans, BigInt *a, word b, word *res);
 //void bnDivInt(BigInt *res, BigInt *a, BigInt *b);
+void bnIntToStr(char *ans, BigInt *a);
--- trunk/no-java/c/BigNum/test_lib.c
+++ trunk/no-java/c/BigNum/test_lib.c
@@ -7,17 +7,8 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 #include "BigInt.h"
-
-void printHex(BigInt *a) {
-    int i;
-    if (a->sign == BN_NEG)
-        printf("-");
-    REPB(i,a->size) {
-        printf("%x ",a->d[i]);
-    }
-    printf("\n");
-}
 
 void testSum() {
     BigInt* a = bnNewBigInt(32,0);
@@ -25,24 +16,48 @@ void testSum() {
     a->d[0] = 0xcfab18e4;
     a->d[1] = 0xffa01172;
     a->size=2;
-    printf("a: "); printHex(a);
+    bnAddInt(b,b,a);
+    printf("a: "); printHex(b);
     bnAddInt(b,a,a);
     printf("2*a: "); printHex(b);
     bnSubInt(b,b,a);
     printf("2*a-a: "); printHex(b);
     bnSubInt(b,b,a);
     printf("a-a: "); printHex(b);
-    bnSubInt(b,b,a);
+    bnSubInt(b,b,a);    
     printf("-a: "); printHex(b);
+    bnShiftLBits(b,a,64);
+    printf("a<<64: "); printHex(b);
+    bnShiftLBits(b,a,36);
+    printf("a<<36: "); printHex(b);
+    bnMulInt(b,a,a);
+    printf("a*a: "); printHex(b);
+    bnMulInt(b,b,b);
+    printf("a^4: "); printHex(b);
     bnDelBigInt(a);
     bnDelBigInt(b);
+}
+
+void fac(int n) {
+    BigInt *r = bnNewBigInt(500,1);
+    BigInt *f = bnNewBigInt(1,1);
+    char ans[2000];
+    int *i = &(f->d[0]);
+    for ( (*i)=1; (*i)<=n; (*i)++ ) {
+        bnMulInt(r,r,f);
+    }
+    bnIntToStr(ans,r);
+    printf("%s\n",ans);
 }
 
 /*
  * 
  */
 int main(int argc, char** argv) {
-    testSum();
+    clock_t t = clock();
+    fac(256);
+    t = clock()-t;
+    printf("Time elapsed: %i tks\n", t);
     return (EXIT_SUCCESS);
 }
 
