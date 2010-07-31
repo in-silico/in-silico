00fb16b918fb5e29d63e4876e813659d839b04dc
santigutierrez1 <santigutierrez1@13b141fe-a120-11dd-a9c7-49dec45ca499>
2010-07-31 07:11:00 +0000

    Cambios en dailyOCR en los ultimos meses

--- trunk/Santiago/dailyOCR/src/control/SistemaEstrategias.java
+++ trunk/Santiago/dailyOCR/src/control/SistemaEstrategias.java
@@ -4,8 +4,6 @@ import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.Calendar;
 
-import control.conexion.ConexionServidorMensajes;
-
 import modelo.Estrategia;
 import modelo.Senal;
 
