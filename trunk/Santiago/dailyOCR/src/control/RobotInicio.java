b4ce9feef799cbd3883e3990ed8be8b5be5d8958
santigutierrez1@gmail.com <santigutierrez1@gmail.com@13b141fe-a120-11dd-a9c7-49dec45ca499>
2010-09-19 21:11:03 +0000

    Cambios menores dailyFX

--- trunk/Santiago/dailyOCR/src/control/RobotInicio.java
+++ trunk/Santiago/dailyOCR/src/control/RobotInicio.java
@@ -1,109 +1,14 @@
 package control;
 import java.awt.AWTException;
-import java.awt.Robot;
-import java.awt.event.InputEvent;
-import java.awt.event.KeyEvent;
-import java.util.Locale;
+import java.io.File;
+import java.io.IOException;
 
 public class RobotInicio 
 {
-	
-	public static void escribir(String s, Robot r)
+	public static void main(String [] args) throws AWTException, InterruptedException, IOException
 	{
-		s = s.toUpperCase().replace("7", "/").replace("6", "&");
-		for(char c : s.toCharArray())
-		{
-			if(c == '/')
-			{
-				r.keyPress(KeyEvent.VK_SHIFT);
-				r.keyPress(KeyEvent.VK_7);
-				r.keyRelease(KeyEvent.VK_7);
-				r.keyRelease(KeyEvent.VK_SHIFT);
-				
-			}
-			else if(c == '.')
-			{
-				r.keyPress(KeyEvent.VK_PERIOD);
-				r.keyRelease(KeyEvent.VK_PERIOD);
-			}
-			else
-			{
-				r.keyPress(c);
-				r.keyRelease(c);
-			}
-		}
-	}
-	public static void main(String [] args) throws AWTException, InterruptedException
-	{
-		Thread.sleep(60000);
-		Robot r = new Robot();
-		r.mouseMove(86, 467);
-		r.mousePress(InputEvent.BUTTON1_MASK);
-		r.mouseRelease(InputEvent.BUTTON1_MASK);
-		r.mousePress(InputEvent.BUTTON1_MASK);
-		r.mouseRelease(InputEvent.BUTTON1_MASK);
-		Thread.sleep(6000);
-		r.keyPress(KeyEvent.VK_S);
-		r.keyRelease(KeyEvent.VK_S);
-		r.keyPress(KeyEvent.VK_U);
-		r.keyRelease(KeyEvent.VK_U);
-		r.keyPress(KeyEvent.VK_D);
-		r.keyRelease(KeyEvent.VK_D);
-		r.keyPress(KeyEvent.VK_O);
-		r.keyRelease(KeyEvent.VK_O);
-		r.keyPress(KeyEvent.VK_SPACE);
-		r.keyRelease(KeyEvent.VK_SPACE);
-		r.keyPress(KeyEvent.VK_S);
-		r.keyRelease(KeyEvent.VK_S);
-		r.keyPress(KeyEvent.VK_U);
-		r.keyRelease(KeyEvent.VK_U);
-		r.keyPress(KeyEvent.VK_ENTER);
-		r.keyRelease(KeyEvent.VK_ENTER);
-		Thread.sleep(6000);
-		r.keyPress(KeyEvent.VK_SHIFT);
-		r.keyPress(KeyEvent.VK_C);
-		r.keyRelease(KeyEvent.VK_C);
-		r.keyRelease(KeyEvent.VK_SHIFT);
-		r.keyPress(KeyEvent.VK_A);
-		r.keyRelease(KeyEvent.VK_A);
-		r.keyPress(KeyEvent.VK_L);
-		r.keyRelease(KeyEvent.VK_L);
-		r.keyPress(KeyEvent.VK_I);
-		r.keyRelease(KeyEvent.VK_I);
-		r.keyPress(KeyEvent.VK_D);
-		r.keyRelease(KeyEvent.VK_D);
-		r.keyPress(KeyEvent.VK_A);
-		r.keyRelease(KeyEvent.VK_A);
-		r.keyPress(KeyEvent.VK_D);
-		r.keyRelease(KeyEvent.VK_D);
-		r.keyPress(KeyEvent.VK_SHIFT);
-		r.keyPress(KeyEvent.VK_I);
-		r.keyRelease(KeyEvent.VK_I);
-		r.keyRelease(KeyEvent.VK_SHIFT);
-		r.keyPress(KeyEvent.VK_N);
-		r.keyRelease(KeyEvent.VK_N);
-		r.keyPress(KeyEvent.VK_G);
-		r.keyRelease(KeyEvent.VK_G);
-		r.keyPress(KeyEvent.VK_E);
-		r.keyRelease(KeyEvent.VK_E);
-		r.keyPress(KeyEvent.VK_S);
-		r.keyRelease(KeyEvent.VK_S);
-		r.keyPress(KeyEvent.VK_I);
-		r.keyRelease(KeyEvent.VK_I);
-		r.keyPress(KeyEvent.VK_S);
-		r.keyRelease(KeyEvent.VK_S);
-		r.keyPress(KeyEvent.VK_ENTER);
-		r.keyRelease(KeyEvent.VK_ENTER);
-		Thread.sleep(6000);
-		escribir("/home/santiago/Desktop/inicio.sh", r);
-		r.keyPress(KeyEvent.VK_SPACE);
-		r.keyRelease(KeyEvent.VK_SPACE);
-		r.keyPress(KeyEvent.VK_SHIFT);
-		r.keyPress(KeyEvent.VK_6);
-		r.keyRelease(KeyEvent.VK_6);
-		r.keyRelease(KeyEvent.VK_SHIFT);
-		r.keyPress(KeyEvent.VK_ENTER);
-		r.keyRelease(KeyEvent.VK_ENTER);
-		Thread.sleep(5000);
+		ProcessBuilder pb = new ProcessBuilder("sudo", "java", "-jar", "dailyOCR.jar");
+		pb.directory(new File("/home/santiago/Desktop/dailyOCR/"));
+		pb.start().waitFor();
 	}
 }
