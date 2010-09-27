d945665cf6a99001dc646c8001aaf4226bf26a96
Santiago Gutierrez Alzate <santigutierrez1@gmail.com>
2010-09-27 05:58:41 +0000

    Cambios menores dailyFX

--- Santiago/dailyOCR/src/control/RobotInicio.java
+++ Santiago/dailyOCR/src/control/RobotInicio.java
@@ -1,13 +1,12 @@
 package control;
-import java.awt.AWTException;
 import java.io.File;
 import java.io.IOException;
 
 public class RobotInicio 
 {
-	public static void main(String [] args) throws AWTException, InterruptedException, IOException
+	public static void main(String [] args) throws InterruptedException, IOException
 	{
-		ProcessBuilder pb = new ProcessBuilder("sudo", "java", "-jar", "dailyOCR.jar");
+		ProcessBuilder pb = new ProcessBuilder("sudo", "java", "-jar", "dailyOCR.jar", "mx1024m", "-Xms512m");
 		pb.directory(new File("/home/santiago/Desktop/dailyOCR/"));
 		pb.start().waitFor();
 		Error.agregar("El programa termino");
