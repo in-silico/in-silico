b6c3de76edee5200094b9543b0d848d814ec26b1
santigutierrez1@gmail.com <santigutierrez1@gmail.com@13b141fe-a120-11dd-a9c7-49dec45ca499>
2010-10-04 06:55:56 +0000

    Agrego el problema de waterloo corregido (accepted) y los dos problemas diarios de practica de simulacion

--- trunk/Maratones/src/UVA/Waterloo_Fall_1/Split.java
+++ trunk/Maratones/src/UVA/Waterloo_Fall_1/Split.java
@@ -2,71 +2,95 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Hashtable;
+import java.util.HashMap;
 import java.util.List;
-import java.util.Map.Entry;
 
 
 public class Split
 {
-	private static Hashtable <Integer, Integer> generarSumas(List <Integer> lista)
+	static HashMap <Integer, Integer> mapa = new HashMap <Integer, Integer> (532000, 1f);
+	
+	static long sumas[] = new long[532000];
+	static int tamSumas = 0;
+	
+	private static void generarSumas(List <Integer> lista)
 	{
-		Hashtable <Integer, Integer> mapa = new Hashtable <Integer, Integer> (1 << lista.size());
-		Hashtable <Integer, Integer> mapaNuevo = new Hashtable <Integer, Integer> (1 << lista.size());
-		int numMax = 0;
-		int numTotal = 0;
-		for(int actual : lista)
+		tamSumas = 0;
+		long actual = lista.get(0);
+		sumas[tamSumas++] = (((actual << 32) >>> 32) | actual << 32);
+		sumas[tamSumas++] = (0L);
+		sumas[tamSumas++] = (((-actual << 32) >>> 32) | actual << 32);
+		for(int i = 1; i < lista.size(); i++)
 		{
-			if(mapa.isEmpty())
+			actual = lista.get(i);
+			int tamanoActual = tamSumas;
+			for(int j = 0; j < tamanoActual; j++)
 			{
-				mapa.put(actual, actual);
-				mapa.put(0, 0);
-				mapa.put(-actual, actual);
-				continue;
+				long esta = sumas[j];
+				int sumaA = (int) ((esta << 32) >>> 32);
+				long acumulado = esta >> 32;
+				long suma = sumaA + actual;
+				long resta = sumaA - actual;
+				sumas[tamSumas++] = (((suma << 32) >>> 32) | (acumulado + actual) << 32);
+				sumas[tamSumas++] = (((resta << 32) >>> 32) | (acumulado + actual) << 32);
 			}
-			numMax = Math.max(numMax, mapa.size());
-			numTotal += mapa.size();
-			mapaNuevo.clear();
-			for(Entry <Integer, Integer> entrada : mapa.entrySet())
-			{
-				int suma = entrada.getKey() + actual;
-				int resta = entrada.getKey() - actual;
-				if(mapaNuevo.containsKey(suma))
-				{
-						mapaNuevo.put(suma, Math.max(mapaNuevo.get(suma), entrada.getValue() + actual));
-				}
-				else
-				{
-						mapaNuevo.put(suma, entrada.getValue() + actual);
-				}
-				if(mapaNuevo.containsKey(resta))
-				{
-						if(resta >= 0)
-						{
-							mapaNuevo.put(resta, Math.max(mapaNuevo.get(resta), entrada.getValue() + actual));
-						}
-				}
-				else
-				{
-						if(resta >= 0)
-						{
-							mapaNuevo.put(resta, entrada.getValue() + actual);
-						}
-				}
-				if(mapaNuevo.containsKey(entrada.getKey()))
-				{
-						mapaNuevo.put(entrada.getKey(), Math.max(mapaNuevo.get(entrada.getKey()), entrada.getValue()));
-				}
-				else
-				{
-						mapaNuevo.put(entrada.getKey(), entrada.getValue());
-				}
-			}
-			Hashtable <Integer, Integer> temp = mapa;
-			mapa = mapaNuevo;
-			mapaNuevo = temp;
 		}
-		return mapa;
+		mapa.clear();
+		for(int i = 0; i < tamSumas; i++)
+		{
+			long l = sumas[i];
+			int sumaA = (int)((l << 32) >>> 32);
+			int acumulado = (int) (l >> 32);
+			Integer posible = mapa.get(sumaA);
+			if(posible != null)
+				mapa.put(sumaA, Math.max(posible, acumulado));
+			else
+				mapa.put(sumaA, acumulado);
+		}
+	}
+	
+	static long sumas1[] = new long[532000];
+	static int tamSumas1 = 0;
+	
+	private static int generarSumas1(List <Integer> lista)
+	{
+		tamSumas1 = 0;
+		long actual = lista.get(0);
+		sumas1[tamSumas1++] = (((actual << 32) >>> 32) | actual << 32);
+		sumas1[tamSumas1++] = (0L);
+		sumas1[tamSumas1++] = (((-actual << 32) >>> 32) | actual << 32);
+		for(int i = 1; i < lista.size(); i++)
+		{
+			actual = lista.get(i);
+			int tamanoActual = tamSumas1;
+			for(int j = 0; j < tamanoActual; j++)
+			{
+				long esta = sumas1[j];
+				int sumaA = (int) ((esta << 32) >>> 32);
+				long acumulado = esta >> 32;
+				long suma = sumaA + actual;
+				long resta = sumaA - actual;
+				sumas1[tamSumas1++] = (((suma << 32) >>> 32) | (acumulado + actual) << 32);
+				sumas1[tamSumas1++] = (((resta << 32) >>> 32) | (acumulado + actual) << 32);
+			}
+		}
+		int mejor = -1;
+		if(mapa.containsKey(0))
+			mejor = Math.max(mejor, mapa.get(0));
+		for(int i = 0; i < tamSumas1; i++)
+		{
+			long l = sumas1[i];
+			int sumaA = (int)((l << 32) >>> 32);
+			int acumulado = (int) (l >> 32);
+			if(sumaA == 0)
+				mejor = Math.max(mejor, acumulado);
+			Integer posible = mapa.get(sumaA);
+			if(posible != null)
+			{
+					mejor = Math.max(mejor, acumulado + posible);
+			}
+		}
+		return mejor;
 	}
 	
 	public static void main(String[] args) throws IOException
@@ -85,21 +109,15 @@ public class Split
 				numeros.add(valorNuevo);
 				acumTotal += valorNuevo;
 			}
-			Hashtable <Integer, Integer> mapaUno = generarSumas(numeros.subList(0, n / 2));
-			Hashtable <Integer, Integer> mapaDos = generarSumas(numeros.subList(n / 2, n));
-			int mejor = -1;
-			if(mapaUno.containsKey(0))
-				mejor = Math.max(mejor, mapaUno.get(0));
-			if(mapaDos.containsKey(0))
-				mejor = Math.max(mejor, mapaDos.get(0));
-			for(Entry <Integer, Integer> entrada : mapaUno.entrySet())
+			if(numeros.size() == 1)
 			{
-				if(mapaDos.containsKey(entrada.getKey()))
-				{
-					mejor = Math.max(mejor, entrada.getValue() + mapaDos.get(entrada.getKey()));
-				}
+				System.out.println(numeros.get(0));
 			}
-			System.out.println((acumTotal - mejor));
+			else
+			{
+				generarSumas(numeros.subList(0, n / 2));
+				System.out.println((acumTotal - generarSumas1(numeros.subList(n / 2, n))));
+			}
 		}
 	}
 }
