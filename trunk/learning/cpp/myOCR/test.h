c01445a5e6425b6e5a024b900c588c0eeb0d2e10
sebasutp <sebasutp@13b141fe-a120-11dd-a9c7-49dec45ca499>
2010-12-08 02:27:26 +0000

    Utilidades para llamada desde la consola

---
+++ trunk/learning/cpp/myOCR/test.h
@@ -0,0 +1,21 @@
+/* 
+ * File:   test.h
+ * Author: seb
+ *
+ * Created on 7 de diciembre de 2010, 09:19 PM
+ */
+
+#ifndef _TEST_H
+#define	_TEST_H
+
+#include "matrix.h"
+
+using namespace MyOCR;
+
+extern bool debug;
+
+Matrix* loadImage(const char *fn);
+int mainTest(int argc, char** argv);
+
+#endif	/* _TEST_H */
+
