cc83048d8ab474b290c660a76c351ba07837b5f0
Santiago Gutierrez Alzate <santigutierrez1@gmail.com>
2011-01-05 19:44:38 +0000

    Cambios menores dailyFX

--- Santiago/dailyOCR/src/modelo/Proveedor.java
+++ Santiago/dailyOCR/src/modelo/Proveedor.java
@@ -87,7 +87,7 @@ public class Proveedor
 					Calendar c = Calendar.getInstance();
 					int hora = c.get(Calendar.HOUR_OF_DAY);
 					int minuto = c.get(Calendar.MINUTE);
-					if(minuto > 10)
+					if(minuto > 30)
 					{
 						mensajeEnviado = chequeoRealizado = false;
 					}
