9c65d2f9bde93be99a5059601d4c6ff39dd5488a
santigutierrez1@gmail.com <santigutierrez1@gmail.com@13b141fe-a120-11dd-a9c7-49dec45ca499>
2011-01-13 23:46:20 +0000

    Cambios menores dailyFX

--- trunk/Santiago/dailyOCR/src/control/AdministradorHilos.java
+++ trunk/Santiago/dailyOCR/src/control/AdministradorHilos.java
@@ -1,6 +1,7 @@
 package control;
 
 import java.util.Calendar;
+import java.util.Random;
 import java.util.concurrent.LinkedBlockingQueue;
 
 import modelo.Par;
@@ -59,7 +60,23 @@ public class AdministradorHilos
 								}
 								if((System.currentTimeMillis() - h.runnable.darUltimaActualizacion()) > h.runnable.darIntervaloActualizacion())
 								{
-									Error.agregar("Error, hilo: " + h.getName() + " no se actualizo en mucho tiempo, intervalo aceptable: " + h.runnable.darIntervaloActualizacion() + ", ultima actualizacion hace: " + (System.currentTimeMillis() - h.runnable.darUltimaActualizacion()));
+									Error.agregar("Error, hilo: " + h.getName() + " no se actualizo en mucho tiempo, intervalo aceptable: " + h.runnable.darIntervaloActualizacion() + ", ultima actualizacion hace: " + (System.currentTimeMillis() - h.runnable.darUltimaActualizacion() + " haciendo debug"));
+									String mensaje = "";
+									long inicio = System.currentTimeMillis();
+									Random random = new Random();
+									for(int i = 0; i < 100; i++)
+									{
+										HiloDaily.sleep(random.nextInt(1000));
+										StackTraceElement[] stack = h.getStackTrace();
+										mensaje += "+ " + (System.currentTimeMillis() - inicio) + " ms :" + (stack.length == 0 ? " null" : " " + stack[0]) + "\n"; 
+									}
+									for(int i = 0; i < 50; i++)
+									{
+										HiloDaily.sleep(random.nextInt(5000));
+										StackTraceElement[] stack = h.getStackTrace();
+										mensaje += "+ " + (System.currentTimeMillis() - inicio) + " ms :" + (stack.length == 0 ? " null" : " " + stack[0]) + "\n"; 
+									}
+									Error.agregar("Debug :\n" + mensaje);
 									Error.reiniciar();
 								}
 							}
