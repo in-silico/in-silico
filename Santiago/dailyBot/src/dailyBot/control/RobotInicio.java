bcb7ec31e65082373e5f95786e3d5120032c0622
Santiago Gutierrez Alzate <santigutierrez1@gmail.com>
2011-01-21 09:15:24 +0000

    Cambios menores dailyBot

--- Santiago/dailyBot/src/dailyBot/control/RobotInicio.java
+++ Santiago/dailyBot/src/dailyBot/control/RobotInicio.java
@@ -6,7 +6,7 @@ public class RobotInicio
 {
 	public static void main(String [] args) throws InterruptedException, IOException
 	{
-		ProcessBuilder pb = new ProcessBuilder("sudo", "java", "-server", "-Xms512m", "-Xmx1536m", "-jar", "dailyBot.jar");
+		ProcessBuilder pb = new ProcessBuilder("sudo", "java", "-server", "-Xms256m", "-Xmx1024m", "-jar", "dailyBot.jar");
 		pb.directory(new File("/home/santiago/Desktop/dailyBot/"));
 		pb.start();
 	}
