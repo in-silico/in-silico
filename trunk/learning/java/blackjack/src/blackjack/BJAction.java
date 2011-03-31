b0492c510f79df62c2ae204f2e932e1baa400f53
sebasutp <sebasutp@13b141fe-a120-11dd-a9c7-49dec45ca499>
2011-03-31 15:39:27 +0000

    Interfaces of BlackJack

---
+++ trunk/learning/java/blackjack/src/blackjack/BJAction.java
@@ -0,0 +1,30 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package blackjack;
+
+/**
+ *
+ * @author seb
+ */
+public class BJAction implements Accion {
+    public static int DEAL=1;
+    public static int STOP=2;
+    int actionType;
+
+    public int compareTo(Accion o) {
+        BJAction a2 = (BJAction)o;
+        return actionType - a2.actionType;
+    }
+
+    int getActionType() {
+        return actionType;
+    }
+
+    public BJAction(int actionType) {
+        this.actionType = actionType;
+    }
+
+}
