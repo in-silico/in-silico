0d8dbb3b7509edf054804ae27f43255a11f1cdc1
Santiago Gutierrez Alzate <santigutierrez1@gmail.com>
2011-04-01 15:33:36 +0000

    Cambios en el anteproyecto (y blackjack)

--- learning/java/blackjack/src/blackjack/Ambiente.java
+++ learning/java/blackjack/src/blackjack/Ambiente.java
@@ -12,8 +12,7 @@ import java.util.LinkedList;
  * @author santiago
  */
 public interface Ambiente {
-    public double getReward(Estado s, Accion a);
-    public Estado nextState(Estado s, Accion a);
+    public Par<Double,Estado> getRewardAndState(Estado s, Accion a);
     public Estado getInitialState();
     public LinkedList<Accion> getAcciones();
     public boolean isTerminalState(Estado s);
