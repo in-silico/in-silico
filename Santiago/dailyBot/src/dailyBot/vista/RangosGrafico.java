640d1bd444d32a0b5ba56d2285d9713f47570d76
Santiago Gutierrez Alzate <santigutierrez1@gmail.com>
2011-04-13 01:17:55 +0000

    Cambios dailyBot

--- Santiago/dailyBot/src/dailyBot/vista/RangosGrafico.java
+++ Santiago/dailyBot/src/dailyBot/vista/RangosGrafico.java
@@ -8,9 +8,11 @@ import java.awt.event.WindowEvent;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.swing.JComponent;
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.JTabbedPane;
 
 import dailyBot.analisis.Indicador;
 import dailyBot.analisis.Rangos;
@@ -18,40 +20,23 @@ import dailyBot.analisis.RegistroHistorial;
 import dailyBot.control.Error;
 import dailyBot.modelo.Estrategia.IdEstrategia;
 import dailyBot.modelo.Par;
+import dailyBot.modelo.Proveedor.IdProveedor;
 
 public class RangosGrafico extends JFrame
 {
 	private static final long serialVersionUID = -3243368885519444393L;
-
-	private ArrayList <RangoGrafico> rangosGrafico = new ArrayList <RangoGrafico> ();
 	
-	public RangosGrafico(final Rangos rangos, List <RegistroHistorial> registros, final IdEstrategia idEstrategia, final Par par, String titulo)
+	public RangosGrafico(final Rangos[] rangos, List <RegistroHistorial> registros, final IdEstrategia idEstrategia, final Par par, String titulo)
 	{
 		super(titulo);
-		GraficaProgreso graficaProgreso = new GraficaProgreso(rangos, registros);
-		final GraficaIndicador graficaIndicador = new GraficaIndicador(Indicador.VIX.darRango().duplicar(), registros, Indicador.VIX, rangos);
-		GraficaHistorial graficaHistorial = new GraficaHistorial(rangos, registros, titulo);
-		JPanel panelRangos = new JPanel();
-		GridLayout gridLayout = new GridLayout();
-		gridLayout.setRows(6);
-		gridLayout.setColumns(1);
-		panelRangos.setLayout(gridLayout);
-		panelRangos.setSize(700, 600);
-		for(Indicador i : Indicador.values())
-		{
-			RangoGrafico r = new RangoGrafico(this, i.darRango().duplicar(), rangos, graficaProgreso, graficaIndicador, graficaHistorial, i);
-			rangosGrafico.add(r);
-			panelRangos.add(r);
-		}
-		graficaIndicador.unico = false;
-		JPanel panelGraficas = new JPanel();
-		panelGraficas.setLayout(new BorderLayout());
-		panelGraficas.add(graficaProgreso, BorderLayout.SOUTH);
-		panelGraficas.add(graficaIndicador, BorderLayout.NORTH);
-		setLayout(new BorderLayout());
-		add(panelRangos, BorderLayout.WEST);
-		add(panelGraficas, BorderLayout.EAST);
 		final JFrame este = this;
+		JTabbedPane panel = new JTabbedPane();
+		for(int i = 0; i < rangos.length; i++)
+		{
+			JComponent actual = new RangosProveedor(rangos[i], registros, idEstrategia, par, titulo);
+			panel.addTab(IdProveedor.values()[i].toString(), actual);
+		}
+		add(panel);
 	    addWindowListener(new WindowAdapter() 
 	    {
 	    	@Override
@@ -60,7 +45,10 @@ public class RangosGrafico extends JFrame
 	    		try
 	    		{
 	    			if(idEstrategia != null && JOptionPane.showConfirmDialog(este, "Guardar el rango?", "Guardar " + idEstrategia + " " + par, JOptionPane.YES_NO_OPTION) == JOptionPane.YES_OPTION)
-	    				VentanaPrincipal.conexion.cambiarRangosEstrategia(idEstrategia.ordinal(), par.ordinal(), rangos);
+	    			{
+	    				for(int i = 0; i < rangos.length; i++)
+	    					VentanaPrincipal.conexion.cambiarRangosProveedor(i, idEstrategia.ordinal(), par.ordinal(), rangos[i]);
+	    			}
 				}
 	            catch (Exception e1)
 	            {        	
@@ -74,9 +62,43 @@ public class RangosGrafico extends JFrame
 		setVisible(true);
 	}
 	
-	public void actualizarTodos()
+	class RangosProveedor extends JPanel
 	{
-		for(RangoGrafico r : rangosGrafico)
-			r.actualizar();
+		private static final long serialVersionUID = -6260738056337041545L;
+		
+		private ArrayList <RangoGrafico> rangosGrafico = new ArrayList <RangoGrafico> ();
+		
+		public RangosProveedor(final Rangos rangos, List <RegistroHistorial> registros, final IdEstrategia idEstrategia, final Par par, String titulo)
+		{
+			GraficaProgreso graficaProgreso = new GraficaProgreso(rangos, registros);
+			final GraficaIndicador graficaIndicador = new GraficaIndicador(Indicador.VIX.darRango().duplicar(), registros, Indicador.VIX, rangos);
+			GraficaHistorial graficaHistorial = new GraficaHistorial(rangos, registros, titulo);
+			JPanel panelRangos = new JPanel();
+			GridLayout gridLayout = new GridLayout();
+			gridLayout.setRows(6);
+			gridLayout.setColumns(1);
+			panelRangos.setLayout(gridLayout);
+			panelRangos.setSize(700, 600);
+			for(Indicador i : Indicador.values())
+			{
+				RangoGrafico r = new RangoGrafico(this, i.darRango().duplicar(), rangos, graficaProgreso, graficaIndicador, graficaHistorial, i);
+				rangosGrafico.add(r);
+				panelRangos.add(r);
+			}
+			graficaIndicador.unico = false;
+			JPanel panelGraficas = new JPanel();
+			panelGraficas.setLayout(new BorderLayout());
+			panelGraficas.add(graficaProgreso, BorderLayout.SOUTH);
+			panelGraficas.add(graficaIndicador, BorderLayout.NORTH);
+			setLayout(new BorderLayout());
+			add(panelRangos, BorderLayout.WEST);
+			add(panelGraficas, BorderLayout.EAST);
+		}
+	
+		public void actualizarTodos()
+		{
+			for(RangoGrafico r : rangosGrafico)
+				r.actualizar();
+		}
 	}
 }
