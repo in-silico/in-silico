8287829dac7575f98c15cfd07b113d3ae7f46739
Santiago Gutierrez Alzate <santigutierrez1@gmail.com>
2011-09-10 14:43:21 +0000

    Subo más algoritmos y otra maraton

--- Maratones/Algoritmos/Estructuras/Fenwick_Tree.cpp
+++ Maratones/Algoritmos/Estructuras/Fenwick_Tree.cpp
@@ -31,3 +31,113 @@ void increase(vector<int> &tree, int k, int inc) {
     for (; k < (int)tree.size(); k |= k + 1)
         tree[k] += inc;
 }
+
+typedef long long lint;
+typedef unsigned long long ulint;
+
+const int MAX = 1050;
+
+int tree[MAX][MAX], lastx, lasty;
+int M[MAX][MAX];
+
+
+int
+readx (int x, int y)
+{
+  int sum = 0;
+  while (x > 0)
+    {
+      sum += tree[x][y];
+      x -= (x & -x);
+    }
+  return sum;
+}
+
+int
+getsum (int x, int y)
+{
+  int sum = 0;
+  while (y > 0)
+    {
+      sum += readx (x, y);
+      y -= (y & -y);
+    }
+  return sum;
+
+}
+
+
+void
+updatey (int x, int y, int val)
+{
+  while (y = lasty)
+    {
+      tree[x][y] += val;
+      y += (y & -y);
+    }
+
+}
+
+void
+update (int x, int y, int val)
+{
+  while (x = lastx)
+    {
+      updatey(x,y,val);
+      x += (x & -x);
+    }
+}
+
+int
+get (int x1, int y1, int x2, int y2)
+{
+  int a = getsum (x1-1 , y1-1 );
+  int b = getsum (x2, y2);
+  int c = getsum (x2, y1 -1);
+  int d = getsum (x1 -1, y2);
+//	printf("%d,%d,%d,%d\n",a,b,c,d);
+  return a + b - c - d;
+}
+
+int
+main ()
+{
+  int no;
+  scanf (" %d", &no);
+  while (no--)
+    {
+      memset (tree, 0, sizeof (tree));
+      int n, i, j, x, u, v, p, q, y;
+      char str[10];
+      scanf (" %d", &n);
+      lastx = lasty = n;
+      while (scanf (" %s", str) != EOF)
+	{
+	  if (strcmp (str, "END") == 0)
+	    break;
+	  if (strcmp (str, "SET") == 0)
+	    {
+	      	scanf (" %d %d %d", &x, &y, &v);
+
+	      	x++, y++;
+		int val=v-get(x,y,x,y);
+		//int val=v-M[x][y];
+//		printf("val=%d,%d\n",val,tree[x][y]);
+
+	        update (x, y, val);
+
+//		printf("after updating %d\n",tree[x][y]);
+	    }
+	  if (strcmp (str, "SUM") == 0)
+	    {
+	      scanf (" %d %d %d %d", &u, &v, &p, &q);
+	       u++,v++,p++, q++;
+	      printf ("%d\n", get (u, v, p, q));
+	    }
+	}
+    }
+	if(no) printf("\n");
+
+  return 0;
+}
+
---
+++ Maratones/Algoritmos/Grafos/assigment_files/salog.js.aspx
@@ -0,0 +1,15 @@
+
+
+var hsUse20Servers = true;
+var hsDayEndsIn = 37465;
+var hsWeekEndsIn = 210265;
+var hsMonthEndsIn = 1851865;
+var hsAnalyticsServer = "tracking.hubspot.com";
+var hsTimeStamp = "2011-09-09 13:35:34";
+var hsIsNewVisitor = 0;
+var hsFirstVisitValue = "http%253A%252F%252Fwww.topcoder.com%252F%7c%7c2011-04-29%252014%253A13%253A10";
+var hsut = 'd6b611f7-6a27-4b09-b9c7-d4fed2f5373f';
+var hsVisitLogOff = true;
+
+
+document.write(unescape("%3Cscript src='" + document.location.protocol + "//" + hs_ppa + "/salog20.js?v=2.13' type='text/javascript'%3E%3C/script%3E"));
