72ab351b6af887cad88c4be34c1e77cf2ac0c87b
sebasutp <sebasutp@13b141fe-a120-11dd-a9c7-49dec45ca499>
2011-10-03 03:04:07 +0000

    Cubic end - new york 2005

--- trunk/Maratones/src/ICPC/2005/Norteamerica/GNYNA/cubic.cpp
+++ trunk/Maratones/src/ICPC/2005/Norteamerica/GNYNA/cubic.cpp
@@ -1,16 +1,19 @@
+
 #include <cstdlib>
 #include <cstdio>
 #include <cstring>
 #include <algorithm>
 #include <cmath>
 
+typedef long long int dword;
+
 using namespace std;
 
-long long totient[15];
+dword totient[15];
 bool debug=false;
 
-long long multmod(long long a, long long b, long long mod) {
-    long long ans=0,c=1,tmp,mod2;
+dword multmod(dword a, dword b, dword mod) {
+    dword ans=0,c=1,tmp,mod2;
     mod2 = mod;
     while (b > 0) {
         tmp = (a*(b%10)) % mod2;
@@ -20,16 +23,16 @@ long long multmod(long long a, long long b, long long mod) {
     return ans;
 }
 
-long long mypow(long long a, int b) {
+dword mypow(dword a, int b) {
     if (b==0) return 1;
-    long long t = mypow(a,b/2);
+    dword t = mypow(a,b/2);
     return t*t*((b%2)==1 ? a : 1);
 }
 
-long long powmod(long long a, long long b, long long mod) {
+dword powmod(dword a, dword b, dword mod) {
     if (b==0) return 1;
 
-    long long t = powmod(a,b/2,mod);
+    dword t = powmod(a,b/2,mod);
     t = multmod(t,t,mod);
     if (b%2 == 1) t = multmod(t,a,mod);
     return t%mod;
@@ -42,39 +45,38 @@ void precompute() {
     }
 }
 
-void extgcd(long long *ans, long long a, long long b) {
+void extgcd(dword *ans, dword a, dword b) {
     if (b==0) {
         ans[0]=1; ans[1]=0;
     } else {
-        long long tmp[2];
-        long long q = a/b, r = a%b;
+        dword tmp[2];
+        dword q = a/b, r = a%b;
         extgcd(tmp, b, r);
         ans[0]=tmp[1]; ans[1]=tmp[0]-q*tmp[1];
     }
 }
 
-long long inverse(long long a, long long mod) {
-    long long tmp[2];
+dword inverse(dword a, dword mod) {
+    dword tmp[2];
     extgcd(tmp, a, mod);
     return ((tmp[0]%mod)+mod)%mod;
 }
 
 int main() {
     int N,e;
-    long long a,x,m,inv3;
+    dword a,x,m,inv3;
     precompute();
     scanf("%d",&N);
     for (int i=0; i<N; i++) {
-        scanf("%lld",&a);
+        scanf("%ld",&a);
         if (a==1) {
             printf("1\n"); continue;
         }
         e = (int)ceil(log10(a));
         m = mypow(10,e);
         inv3 = inverse(3, totient[e]); //inverse of cube with respect totient
-        if (debug) printf("inv3=%lld, a^%lld=%lld\n",inv3, totient[e], powmod(a,totient[e],m));
+        if (debug) printf("inv3=%ld, a^%ld=%ld\n",inv3, totient[e], powmod(a,totient[e],m));
         x = powmod(a, inv3, m);
-        printf("%lld",x);
-        if (i!=(N-1)) printf("\n");
+        printf("%ld\n",x);
     }
 }
