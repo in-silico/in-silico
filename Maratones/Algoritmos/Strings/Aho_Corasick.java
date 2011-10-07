0f07eaa119291cdf2a7a7506a8adc9d67c6a4456
Santiago Gutierrez Alzate <santigutierrez1@gmail.com>
2011-10-07 16:48:26 +0000

    Subo un problema más maratones

--- Maratones/Algoritmos/Strings/Aho_Corasick.java
+++ Maratones/Algoritmos/Strings/Aho_Corasick.java
@@ -1,145 +1,251 @@
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
+import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.Comparator;
 import java.util.Queue;
+import java.util.StringTokenizer;
 
-public class Aho_Corasick {
-  static class PMA {
-    PMA next[];
-    PMA fail;
-    List<Integer> accept;
-    PMA() {
-      next = new PMA[256]; // Se puede cambiar por un Mapa
-      accept = new ArrayList<Integer>(); // Se puede cambiar por una linked list especial
-    }                                    // en la cual addAll sea constante
-  }
+public class Aho_Corasick 
+{
+	static class Scanner
+	{
+		BufferedReader br;
+		StringTokenizer st;
+		
+		public Scanner()
+		{
+	    	System.setOut(new PrintStream(new BufferedOutputStream(System.out), true));
+			br = new BufferedReader(new InputStreamReader(System.in));
+		}
+		
+		public String next()
+		{
+			while(st == null || !st.hasMoreTokens())
+			{
+				try { st = new StringTokenizer(br.readLine()); }
+				catch(Exception e) { throw new RuntimeException(); }
+			}
+			return st.nextToken();
+		}
 
-  static PMA buildPMA(char p[][], int size) {
-    PMA root = new PMA();
-    for (int i = 0; i < size; i++) {
-      PMA t = root;
-      for (int j = 0; j < p[i].length; ++j) {
-        char c = p[i][j];
-        if (t.next[c] == null)
-          t.next[c] = new PMA();
-        t = t.next[c];
-      }
-      t.accept.add(i);
-    }
-    for(int c = 'a' ; c <= 'z' ; c++)
-      if(root.next[c] == null)
-        root.next[c] = root;
-    Queue<PMA> Q = new LinkedList<PMA>();
-    for (int c = 'a'; c <= 'z'; c++) {
-      if (root.next[c] != root) {
-        root.next[c].fail = root;
-        Q.add(root.next[c]);
-      }
-    }
-    while (!Q.isEmpty()) {
-      PMA t = Q.poll();
-      for (int c = 'a'; c <= 'z'; c++) {
-        if (t.next[c] != null) {
-          Q.add(t.next[c]);
-          PMA r = t.fail;
-          while (r.next[c] == null)
-            r = r.fail;
-          t.next[c].fail = r.next[c];
-          t.next[c].accept.addAll(r.next[c].accept); // ojo!! con esta linea, a matchea 10 veces en aaaaaaaaaa
-        }
-      }
-    }
-    return root;
-  }
+		public int nextInt()
+		{
+			return Integer.parseInt(next());
+		}
+		
+		public double nextDouble()
+		{
+			return Double.parseDouble(next());
+		}
+		
+		public String nextLine()
+		{
+			st = null;
+			try { return br.readLine(); }
+			catch(Exception e) { throw new RuntimeException(); }
+		}
+		
+		public boolean endLine()
+		{
+			try 
+			{
+				String next = br.readLine();
+				while(next != null && next.trim().isEmpty())
+					next = br.readLine();
+				if(next == null)
+					return true;
+				st = new StringTokenizer(next);
+				return !st.hasMoreTokens();
+			}
+			catch(Exception e) { throw new RuntimeException(); }
+		}
+	}	
+	
+	static class Nodo
+	{
+		Nodo siguiente;
+		int valor;
 
-  static void match(char t[], PMA v, int result[]) {
-    int n = t.length;
-    for (int i = 0; i < n; i++) {
-      char c = t[i];
-      while (v.next[c] == null)
-        v = v.fail;
-      v = v.next[c];
-      for (int j : v.accept){
-        result[j] = 1;
-      }
-    }
-  }
+		public Nodo(int v)
+		{
+			valor = v;
+		}
+	}
 
-  static int kmp(char t[], char p[], int fail[]) {
-    int n = t.length;
-    int m = p.length;
-    for (int i = 0, k = 0; i < n; i++) {
-      while (k >= 0 && p[k] != t[i])
-        k = fail[k];
-      if (++k >= m) {
-        return 1;
-      }
-    }
-    return 0;
-  }
+	static class PMA 
+	{
+		PMA next[];
+		PMA fail;
+		Nodo accept;
+		Nodo ultimo;
 
-  static int[] buildFail(char p[]) {
-    int m = p.length;
-    int fail[] = new int[m + 1];
-    int j = fail[0] = -1;
-    for (int i = 1; i <= m; i++) {
-      while (j >= 0 && p[j] != p[i - 1])
-        j = fail[j];
-      fail[i] = ++j;
-    }
-    return fail;
-  }
+		PMA() 
+		{
+			next = new PMA[27];
+		}
 
-  void getRay(char grid[][], int x, int y, char ray[]) {
-    int cx = 0;
-    int cy = 0;
-    for (int i = 0; i < ray.length; i++) {
-      ray[i] = grid[cy][cx];
-      cx += x;
-      cy += y;
-      if (cy >= grid.length)
-        cy -= grid.length;
-      if (cx >= grid[0].length)
-        cx -= grid[0].length;
-    }
-  }
+		void add(int n)
+		{
+			if(accept == null)
+				accept = ultimo = new Nodo(n);
+			else
+			{
+				ultimo.siguiente = new Nodo(n);
+				ultimo = ultimo.siguiente;
+			}
+		}
 
-  static int gcd(int a, int b) {
-    if (b == 0)
-      return a;
-    return gcd(b, a % b);
-  }
+		void addAll(PMA other)
+		{
+			if(other.accept == null)
+				return;
+			if(accept == null)
+			{
+				accept = other.accept;
+				ultimo = other.ultimo;
+			}
+			else
+			{
+				ultimo.siguiente = other.accept;
+				ultimo = other.ultimo;
+			}
+		}
 
-  public int[] countRays(String[] g, String[] words, int k) {
-    int H = g.length;
-    int W = g[0].length();
-    int wnum = words.length;
-    char grid[][] = new char[H][W];
-    for (int i = 0; i < grid.length; i++)
-      grid[i] = g[i].toCharArray();
-    boolean check[][] = new boolean[H][W];
-    int memo[][][] = new int[H][W][wnum];
-    int res[] = new int[wnum];
-    char ray[] = new char[H * W + 50];
-    char ws[][] = new char[wnum][];
-    for (int i = 0; i < ws.length; i++)
-      ws[i] = words[i].toCharArray();    
-    PMA pma = buildPMA(ws, wnum);
-    for (int x = 0; x <= k; x++) {
-      for (int y = 0; y <= k; y++) {
-        int gcd = gcd(x, y);
-        if (gcd != 1) continue;
-        int a = x % W;
-        int b = y % H;
-        if (!check[b][a]) {
-          getRay(grid, a, b, ray);
-          match(ray, pma, memo[b][a]);
-          check[b][a] = true;
-        }
-        for (int i = 0; i < wnum; i++)
-          res[i] += memo[b][a][i];
-      }
-    }
-    return res;
-  }
+		void match(char t[], int inicio, int tam, int dp[], int nuevo) 
+		{
+			int n = inicio + tam;
+			PMA v = this;
+			for (int i = inicio; i < n; i++)
+			{
+				int c = t[i] - 'a';
+				while (v.next[c] == null)
+					v = v.fail;
+				v = v.next[c];
+				Nodo actual = v.accept;
+				while(actual != null) 
+				{
+					dp[actual.valor] = Math.max(dp[actual.valor], nuevo);
+					actual = actual.siguiente;
+				}
+			}
+		}
+	}
+	
+	static PMA buildPMA(char p[][], int size) 
+	{
+		PMA root = new PMA();
+		for (int i = 0; i < size; i++) 
+		{
+			PMA t = root;
+			for (int j = 0; j < p[i].length; j++) 
+			{
+				int c = p[i][j] - 'a';
+				if (t.next[c] == null)
+					t.next[c] = new PMA();
+				t = t.next[c];
+			}
+			t.add(i);
+		}
+		for(int c = 0; c <= 25; c++)
+			if(root.next[c] == null)
+				root.next[c] = root;
+		Queue <PMA> Q = new ArrayDeque <PMA> ();
+		for(int c = 0; c <= 25; c++)
+		{
+			if(root.next[c] != root) 
+			{
+				root.next[c].fail = root;
+				Q.add(root.next[c]);
+			}
+		}
+		while(!Q.isEmpty()) 
+		{
+			PMA t = Q.poll();
+			for (int c = 0; c <= 25; c++) 
+			{
+				if (t.next[c] != null) 
+				{
+					Q.add(t.next[c]);
+					PMA r = t.fail;
+					while (r.next[c] == null)
+						r = r.fail;
+					t.next[c].fail = r.next[c];
+					t.next[c].addAll(r.next[c]);
+				}
+			}
+		}
+		return root;
+	}
+
+	static int kmp(char t[], char p[], int fail[]) 
+	{
+		int n = t.length;
+		int m = p.length;
+		for (int i = 0, k = 0; i < n; i++) 
+		{
+			while (k >= 0 && p[k] != t[i])
+				k = fail[k];
+			if (++k >= m) 
+				return 1;
+		}
+		return 0;
+	}
+
+	static int[] buildFail(char p[]) 
+	{
+		int m = p.length;
+		int fail[] = new int[m + 1];
+		int j = fail[0] = -1;
+		for (int i = 1; i <= m; i++) 
+		{
+			while (j >= 0 && p[j] != p[i - 1])
+				j = fail[j];
+			fail[i] = ++j;
+		}
+		return fail;
+	}
+	
+	public static void main(String[] args) throws FileNotFoundException
+	{
+		int[] dp = new int[10100];
+		Scanner sc = new Scanner();
+		String[] palabrasS = new String[10100];
+		char[][] palabras = new char[10100][];
+		while(true)
+		{
+			int n = sc.nextInt();
+			if(n == 0)
+				break;
+			for(int i = 0; i < n; i++)
+			{
+				palabrasS[i] = sc.next();
+				dp[i] = 1;
+			}
+			Arrays.sort(palabrasS, 0, n, new Comparator<String>()
+					{
+						public int compare(String o1, String o2)
+						{
+							return o2.length() - o1.length();
+						}
+					});
+			for(int i = 0; i < n; i++)
+				palabras[i] = palabrasS[i].toCharArray();
+			PMA raiz = buildPMA(palabras, n);
+			int mejor = 0;
+			for(int i = 0; i < n; i++)
+			{
+				int dpSig = dp[i] + 1;
+				raiz.match(palabras[i], 1, palabras[i].length - 1, dp, dpSig);
+				raiz.match(palabras[i], 0, palabras[i].length - 1, dp, dpSig);
+				mejor = Math.max(mejor, dp[i]);
+			}
+			System.out.println(mejor);
+			if(n > 100)
+				System.gc();
+		}
+	}
 }
