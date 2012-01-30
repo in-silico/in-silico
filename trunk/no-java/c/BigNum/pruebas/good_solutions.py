4b196065b49095f6011cdfe1c8951f8c77eddfa7
santigutierrez1@gmail.com <santigutierrez1@gmail.com@13b141fe-a120-11dd-a9c7-49dec45ca499>
2012-01-30 23:54:04 +0000

    Subo libreria funcionando

--- trunk/no-java/c/BigNum/pruebas/good_solutions.py
+++ trunk/no-java/c/BigNum/pruebas/good_solutions.py
@@ -6,19 +6,11 @@
 
 import sys
 
-def modulo(a, b):
-	q = a / b
-	return a - q * b
-	
 for i in sys.stdin:
 	lista = i.split()
 	base = int(lista[1])
 	exponent = int(lista[2])
-	mod = int(lista[3])
-	ans = 1
-	while(exponent > 0): 
-		if((exponent & 1) == 1):
-			ans = modulo((ans * base), mod)
-		exponent >>= 1
-		base = modulo((base * base), mod)			
-	print ans
+	if(lista[0] == "%"):
+		print base % exponent
+	if(lista[0] == "/"):
+		print base / exponent
