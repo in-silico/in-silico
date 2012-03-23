f16c0453fe0d3b0c9a8b64d916f0b02e30cacdcd
sebasutp <sebasutp@users.noreply.github.com>
2012-03-23 23:00:19 +0000

    Algunas correcciones al karatsuba

--- no-java/c/BigNum/pruebas/good_solutions.py
+++ no-java/c/BigNum/pruebas/good_solutions.py
@@ -8,14 +8,19 @@ import sys
 
 for i in sys.stdin:
 	lista = i.split()
-	base = int(lista[1], 16)
-	exponent = int(lista[2], 16)
+	#base = int(lista[1], 16)
+	base = int(lista[1])
+	#exponent = int(lista[2], 16)
+	exponent = int(lista[2])
 	if(lista[0] == "%"):
-		print hex(base % exponent)[2:-1]
+		#print hex(base % exponent)[2:-1]
+		print base % exponent
 	elif(lista[0] == "/"):
-		print hex(base / exponent)[2:-1]
+		#print hex(base / exponent)[2:-1]
+		print base / exponent
 	elif(lista[0] == "*"):
-		print hex(base * exponent)[2:-1]
+		#print hex(base * exponent)[2:-1]
+		print base * exponent
 	elif(lista[0] == "^"):
 		mod = int(lista[3], 16)
 		ans = 1
@@ -24,4 +29,5 @@ for i in sys.stdin:
 				ans = (ans * base) % mod
 			base = (base * base) % mod
 			exponent = exponent >> 1
-		print hex(ans)[2:-1]
+		#print hex(ans)[2:-1]
+		print ans
