40d5b33cfaf5f2a48eb771bce98992db9d3620d4
sebasutp <sebasutp@users.noreply.github.com>
2012-04-04 17:01:15 +0000

    moving source files to branch

--- CompiladorMJ/src/MJ/Error/SyntacticalError.py
+++
@@ -1,9 +0,0 @@
-from MJ.Error.CompilerError import CompilerError
-
-# Error sintactico
-
-class SyntacticalError(CompilerError):
-
-    def __init__(self, args):
-        print(args)
-        self.mensaje = args
--- CompiladorMJ/src/MJ/run.bat
+++
@@ -1,2 +0,0 @@
-@ECHO OFF
-java -jar microLIR.jar C:\Users\Santiago\workspace\microLIR\test\test_empty_string.lir
