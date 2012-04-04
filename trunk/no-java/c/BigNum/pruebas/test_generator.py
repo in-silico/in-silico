31a705a205503cc655c0161ec2cf1d2855170e3c
aleagues <aleagues@13b141fe-a120-11dd-a9c7-49dec45ca499>
2012-04-04 22:12:55 +0000

    fixed bug in good_solutions.py

--- trunk/no-java/c/BigNum/pruebas/test_generator.py
+++ trunk/no-java/c/BigNum/pruebas/test_generator.py
@@ -2,14 +2,22 @@
 
 import random
 
-test_cases=10000
+test_cases=1000
 lower_bound=2**32
 upper_bound=2**1024
 
+opcodes=['+','-','/','%','^']
+
 
 for i in range(0,test_cases):
+    op=opcodes[random.randint(0,len(opcodes)-1)]
     a=random.randint(lower_bound,upper_bound)
-    b=random.randint(lower_bound,2**1024)
-    print "*",a,b
+    b=random.randint(lower_bound,upper_bound)
+    print op,a,b,
+    if (op=='^'):
+        c=random.randint(lower_bound,upper_bound*2)
+        print c
+    else:
+        print
 
 
